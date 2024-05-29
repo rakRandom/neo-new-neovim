5d9578c84451a8922373fc7057293458ffd7c850
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-05-29 03:11:21 -0300

    feat: add compile.py code

--- compile.py
+++ compile.py
@@ -1,3 +1,21 @@
 """
-Todo: Create a code to compile automatically the project
+Compile automatically the project
 """
+
+import os
+
+
+def run():
+    executable_name: str = "main"
+    files_to_compile: list[str] = []
+
+    for dirpath, _, filenames in os.walk("."):
+        for filename in filenames:
+            if filename.endswith(".cpp") and dirpath.startswith(".\\src"):
+                files_to_compile.append(f"{dirpath.replace('\\', '/')}/{filename}")
+    
+    os.system(f"g++ {' '.join(files_to_compile)} -o {executable_name}")
+
+
+if __name__ == "__main__":
+    run()
